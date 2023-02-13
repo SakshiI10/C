a224d5a8b63b08bccf3d5fb820b0870d30c4fcbf
Sakshi <iamsakshiingole@gmail.com>
2023-02-12 21:28:18 -0500

    Initial commit

---
+++ Assignments/2Assg.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+// int main(){
+//     int a, b, c;
+//     printf("Enter a number=");
+//     scanf("%d",&a);
+//     printf("Enter sec number=");
+//     scanf("%d",&b);
+//     c=b;
+//     b=a;
+//     a=c;
+//     printf("After swapping a and b are: %d %d",c, b);
+//     return 0;
+// }
+
+int main(){
+    int a, b, c;
+    printf("Enter a number=");
+    scanf("%d",&a);
+    printf("Enter sec number=");
+    scanf("%d",&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After swapping a and b are: %d %d",a, b);
+    return 0;
+}
