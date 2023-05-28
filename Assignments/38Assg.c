baf4106b2b73fc1780ba2e3c9dec1b860e8e7f2f
Sakshi <iamsakshiingole@gmail.com>
2023-05-27 22:31:05 -0400

    Changes done

--- Assignments/38Assg.c
+++ Assignments/38Assg.c
@@ -1,2 +1,27 @@
-//Write a C Program to find largest element of an array.
+// Write a C Program to find largest element of an array.
 
+#include <stdio.h>
+
+int main()
+{
+    int size, i, largest;
+    printf("Enter size of an array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter elements of an array: ");
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0];
+    for (i = 1; i < size; i++)
+    {
+        if (largest < arr[i])
+            largest = arr[i];
+    }
+    printf("Largest element: %d", largest);
+    return 0;
+}
--- Assignments/tempCodeRunnerFile.c
+++ Assignments/tempCodeRunnerFile.c
@@ -1,13 +1,5 @@
-#include<stdio.h>
-// int main(){
-//     int a, b, c;
-//     printf("Enter a number=");
-//     scanf("%d",&a);
-//     printf("Enter sec number=");
-//     scanf("%d",&b);
-//     c=b;
-//     b=a;
-//     a=c;
-//     printf("After swapping a and b are: %d %d",c, b);
-//     return 0;
-// }
+int size, i, largest;
+    printf("Enter size of an array: ");
+    scanf("%d", &size);
+
+    int arr[size];
